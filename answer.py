7893d3b691dc91440bd83398eda1699e466d2e03
Akbar Khan <akbar.khan@enine.school>
2024-01-05 15:16:06 +0530

    Commit 1

--- answer.py
+++ answer.py
@@ -1,2 +1,23 @@
+"""
+    Module name :- answer
+    Method(s) :- answer()
+"""
+
+from solver import solver
+
+
 def answer():
-    return
+    """
+    Finds factorial of n.
+
+    Args:-
+        n(int) :- An integer number.
+
+    Return
+        Factorial of n.
+    """
+    return solver(100)
+
+
+if __name__ == "__main__":
+    print(f"answer() = {answer()}")
--- solver.py
+++ solver.py
@@ -1,2 +1,36 @@
-def solver():
-    return
+"""
+    Module name :- solver
+    Method(s) :- solver()
+"""
+
+
+def solver(n):
+    """
+    Finds factorial of n.
+
+    Args:-
+        n(int) :- An integer number.
+
+    Return
+        Factorial of n.
+    """
+    product = "1"
+
+    for i in range(2, n + 1):
+        carry = 0
+        product_reverse_copy = product[::-1]
+        product = ""
+
+        for digit in product_reverse_copy:
+            prod = carry + int(digit) * i
+            product = str(prod % 10) + product
+            carry = prod // 10
+
+        if carry:
+            product = str(carry) + product
+
+    return product
+
+
+if __name__ == "__main__":
+    print(f"solver(10) = {solver(10)}")
